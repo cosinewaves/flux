9312c78131ad9e6d5171cf407c2e9c8fb24fad76
cosinewaves <elkhaadnan@gmail.com>
2025-05-12 22:18:37 +0100

    update internalTypings

--- src/flux/internalTypings.lua
+++ src/flux/internalTypings.lua
@@ -1,11 +1,21 @@
+--!strict
+-- internalTypings.lua
+
 export type unit = {
   __index: unit,
   state: string,
-  states: { [string]: component },
+
   addState: (
-    self: unit, name: string, onEnter: (self: component?) -> ()?, onExit: (self: component?) -> ()?
+    self: unit,
+    name: string,
+    onEnter: (self: component?) -> ()?,
+    onExit: (self: component?) -> ()?
   ) -> (),
-  changeState: (self: unit, newState: string) -> ()
+
+  changeState: (
+    self: unit,
+    newState: string
+  ) -> ()
 }
 
 export type component = {
